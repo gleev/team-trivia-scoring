a87c98853ee6bdb0741371246e3e94372c8dbbf7
Joel Clermont <joel@orionweb.net>
2012-09-18 19:20:52 -0500

    added navbar, ability to set # teams, default of 10 teams

---
+++ js/scoring.js
@@ -0,0 +1,31 @@
+var Team = function (leagueId, name) {
+    
+    this.leagueId = leagueId;
+    this.name     = name;
+    
+};
+
+var scoringModel = function () {
+    
+    this.defaultRows = 10;
+    
+    this.teams = ko.observableArray();
+    
+    this.scores = ko.observableArray(new Array(this.defaultRows));
+    
+    this.setRows = function(element) {
+        numRows = element.numRows.value;
+        
+        while (this.scores().length > numRows) {
+            this.scores.pop();
+        }
+        
+        while (this.scores().length < numRows) {
+            this.scores.push('');
+        }
+        
+    }
+    
+};
+
+ko.applyBindings(new scoringModel);
