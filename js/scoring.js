3eb98d84c0fe28cf81c108360e1900df1e08ec50
Joel Clermont <joel@orionweb.net>
2012-09-18 21:00:59 -0500

    wired up logic for loading team name from league ID

--- js/scoring.js
+++ js/scoring.js
@@ -1,32 +1,40 @@
-var Team = function (leagueId, name) {
-    
-    this.leagueId = leagueId;
-    this.name     = name;
-    
+var teams = [];
+teams[1020] = 'Insert Clever Name Here';
+teams[1021] = 'Trivia Busters';
+teams[1022] = 'Show Me On The Doll Where HE Touched You';
+
+var Score = function () {
+    this.leagueId = ko.observable("");
+    this.teamName = ko.computed(function() {
+        return teams[this.leagueId()];
+    }, this);
 };
 
 var scoringModel = function () {
     
-    this.defaultRows = 10;
+    this.defaultRows = 1;
     this.validScores = [-5, -3, -1, 0, 1, 3, 5];
     
     this.teams = ko.observableArray();
     
-    this.scores = ko.observableArray(new Array(this.defaultRows));
+    this.scores = ko.observableArray();
     
-    this.setRows = function(element) {
+    this.setRowsFromForm = function(element) {
         numRows = element.numRows.value;
-        
+        this.setRows(numRows);
+    }
+    
+    this.setRows = function(numRows) {
         while (this.scores().length > numRows) {
             this.scores.pop();
         }
         
         while (this.scores().length < numRows) {
-            this.scores.push('');
-        }
-        
+            this.scores.push(new Score());
+        }        
     }
     
+    this.setRows(this.defaultRows)
 };
 
 ko.applyBindings(new scoringModel);
