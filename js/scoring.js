e746d2067e360d6fdd9fcf0b81f99aa992f0b0df
Joel Clermont <jclermont@gmail.com>
2012-12-07 17:34:41 -0600

    do not consider 0 when testing for duplicate scores

--- js/scoring.js
+++ js/scoring.js
@@ -90,51 +90,51 @@ var Score = function () {
     }
     
     self.isQ2Dupe = function() {
-        return (self.q2()['value'] != '' && (Math.abs(self.q2()['value']) == Math.abs(self.q1()['value'])));
+        return (self.q2()['value'] != '' && self.q2()['value'] != 0 && (Math.abs(self.q2()['value']) == Math.abs(self.q1()['value'])));
     }
     
     self.isQ3Dupe = function() {
-        return (self.q3()['value'] != '' && ((Math.abs(self.q3()['value']) == Math.abs(self.q1()['value'])) || (Math.abs(self.q3()['value']) == Math.abs(self.q2()['value']))));
+        return (self.q3()['value'] != '' && self.q3()['value'] != 0 && ((Math.abs(self.q3()['value']) == Math.abs(self.q1()['value'])) || (Math.abs(self.q3()['value']) == Math.abs(self.q2()['value']))));
     }
     
     self.isQ5Dupe = function() {
-        return (self.q5()['value'] != '' && (Math.abs(self.q5()['value']) == Math.abs(self.q4()['value'])));
+        return (self.q5()['value'] != '' && self.q5()['value'] != 0 && (Math.abs(self.q5()['value']) == Math.abs(self.q4()['value'])));
     }
     
     self.isQ6Dupe = function() {
-        return (self.q6()['value'] != '' && ((Math.abs(self.q6()['value']) == Math.abs(self.q4()['value'])) || (Math.abs(self.q6()['value']) == Math.abs(self.q5()['value']))));
+        return (self.q6()['value'] != '' && self.q6()['value'] != 0 && ((Math.abs(self.q6()['value']) == Math.abs(self.q4()['value'])) || (Math.abs(self.q6()['value']) == Math.abs(self.q5()['value']))));
     }
     
     self.isQ8Dupe = function() {
-        return (self.q8()['value'] != '' && (Math.abs(self.q8()['value']) == Math.abs(self.q7()['value'])));
+        return (self.q8()['value'] != '' && self.q8()['value'] != 0 && (Math.abs(self.q8()['value']) == Math.abs(self.q7()['value'])));
     }
     
     self.isQ9Dupe = function() {
-        return (self.q9()['value'] != '' && ((Math.abs(self.q9()['value']) == Math.abs(self.q7()['value'])) || (Math.abs(self.q9()['value']) == Math.abs(self.q8()['value']))));
+        return (self.q9()['value'] != '' && self.q9()['value'] != 0 && ((Math.abs(self.q9()['value']) == Math.abs(self.q7()['value'])) || (Math.abs(self.q9()['value']) == Math.abs(self.q8()['value']))));
     }
 
     self.isQ12Dupe = function() {
-        return (self.q12()['value'] != '' && (Math.abs(self.q12()['value']) == Math.abs(self.q11()['value'])));
+        return (self.q12()['value'] != '' && self.q12()['value'] != 0 && (Math.abs(self.q12()['value']) == Math.abs(self.q11()['value'])));
     }
     
     self.isQ13Dupe = function() {
-        return (self.q13()['value'] != '' && ((Math.abs(self.q13()['value']) == Math.abs(self.q11()['value'])) || (Math.abs(self.q13()['value']) == Math.abs(self.q12()['value']))));
+        return (self.q13()['value'] != '' && self.q13()['value'] != 0 && ((Math.abs(self.q13()['value']) == Math.abs(self.q11()['value'])) || (Math.abs(self.q13()['value']) == Math.abs(self.q12()['value']))));
     }
     
     self.isQ15Dupe = function() {
-        return (self.q15()['value'] != '' && (Math.abs(self.q15()['value']) == Math.abs(self.q14()['value'])));
+        return (self.q15()['value'] != '' && self.q15()['value'] != 0 && (Math.abs(self.q15()['value']) == Math.abs(self.q14()['value'])));
     }
     
     self.isQ16Dupe = function() {
-        return (self.q16()['value'] != '' && ((Math.abs(self.q16()['value']) == Math.abs(self.q14()['value'])) || (Math.abs(self.q16()['value']) == Math.abs(self.q15()['value']))));
+        return (self.q16()['value'] != '' && self.q16()['value'] != 0 && ((Math.abs(self.q16()['value']) == Math.abs(self.q14()['value'])) || (Math.abs(self.q16()['value']) == Math.abs(self.q15()['value']))));
     }
     
     self.isQ18Dupe = function() {
-        return (self.q18()['value'] != '' && (Math.abs(self.q18()['value']) == Math.abs(self.q17()['value'])));
+        return (self.q18()['value'] != '' && self.q18()['value'] != 0 && (Math.abs(self.q18()['value']) == Math.abs(self.q17()['value'])));
     }
     
     self.isQ19Dupe = function() {
-        return (self.q19()['value'] != '' && ((Math.abs(self.q19()['value']) == Math.abs(self.q17()['value'])) || (Math.abs(self.q19()['value']) == Math.abs(self.q18()['value']))));
+        return (self.q19()['value'] != '' && self.q19()['value'] != 0 && ((Math.abs(self.q19()['value']) == Math.abs(self.q17()['value'])) || (Math.abs(self.q19()['value']) == Math.abs(self.q18()['value']))));
     }
 };
 
@@ -452,10 +452,12 @@ $(function() {
         if (e.keyCode == 39) {
             var nextIndex = parseInt($(":focus").attr('tabindex')) + 1;
             $('[tabindex="' + nextIndex + '"]').focus();
+            return false();
         }
         if (e.keyCode == 37) {
             var previousIndex = parseInt($(":focus").attr('tabindex')) - 1;
             $('[tabindex="' + previousIndex + '"]').focus();
+            return false();
         }
     });
 
